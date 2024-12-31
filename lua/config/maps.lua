f47628e82a64264921f05bb8b1416c392cc7d8f7
MysterySkull <guillaume.hernandez.1@gmail.com>
2024-12-31 18:39:39 +0100

    add keymaps

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -3,4 +3,13 @@ vim.g.mapleader = " "
 -- Toggle NvimTree to see file explorer
 vim.keymap.set("n", "<leader>fe", vim.cmd.NvimTreeToggle)
 
+-- Switch between windows inside NeoVim
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-k>", "<C-w>k") 
+vim.keymap.set("n", "<C-j>", "<C-w>j") 
 
+-- Split and vsplit windows and close windows
+vim.keymap.set("n", "<leader>s", vim.cmd.split)
+vim.keymap.set("n", "<leader>vs", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>c", vim.cmd.close)
