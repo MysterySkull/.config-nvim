b9ebc0f7cb99cf9a2dde9d982faca78af5178bf2
MysterySkull <guillaume.hernandez.1@gmail.com>
2025-01-02 16:50:45 +0100

    minor mod

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -9,8 +9,8 @@ return {
                offsets = {
                   {
                      filetype = "NvimTree",
-                     text = "File Explorer",
-                     text_align = "left",
+                     --text = "File Explorer",
+                     --text_align = "left",
                      separator = true
                   }
                },
