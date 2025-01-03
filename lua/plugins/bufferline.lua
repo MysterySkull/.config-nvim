604c9c6b83d4f5194157d4e8145887dce3af36d4
MysterySkull <guillaume.hernandez.1@gmail.com>
2025-01-03 21:59:56 +0100

    autocompletion and stuff added

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,7 @@
 return {
    {
-      'akinsho/bufferline.nvim', 
-      version = "*", 
+      'akinsho/bufferline.nvim',
+      version = "*",
       dependencies = 'nvim-tree/nvim-web-devicons',
       config = function()
          require("bufferline").setup{
@@ -15,6 +15,11 @@ return {
                   }
                },
                separator_style = "slant",
+               diagnostics = 'nvim_lsp',
+               diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                  local icon = level:match("error") and " " or " "
+                  return " " .. icon .. count
+               end
             }
          }
       end,
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,24 +1,64 @@
 return {
    {
       "neovim/nvim-lspconfig",
-      dependencies = { 
-         "folke/lazydev.nvim",
-         ft = "lua", -- only load on lua files
-         opts = {
-            library = {
-               -- See the configuration section for more details
-               -- Load luvit types when the `vim.uv` word is found
-               { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      dependencies = {
+         -- main one
+         { "ms-jpq/coq_nvim", branch = "coq" },
+
+         -- 9000+ Snippets
+         { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+         -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+         -- Need to **configure separately**
+         { 'ms-jpq/coq.thirdparty', branch = "3p" },
+         -- - shell repl
+         -- - nvim lua api
+         -- - scientific calculator
+         -- - comment banner
+         -- - etc
+         {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+               library = {
+                  -- See the configuration section for more details
+                  -- Load luvit types when the `vim.uv` word is found
+                  { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+               },
             },
          },
       },
-
-      config = function()
-         require('lspconfig').lua_ls.setup{
-            settings = {
-               Lua = {}
+      init = function()
+         vim.g.coq_settings = {
+            auto_start = 'shut-up', -- if you want to start COQ at startup
+            -- Your COQ settings here
+            display = {
+               pum = {
+                  source_context = {"",""}
+               }
+            },
+            keymap = {
+               bigger_preview = 'null',
+               jump_to_mark = 'null',
             }
          }
       end,
-   }
+
+      config = function()
+         require('lspconfig').lua_ls.setup({
+            settings = {
+               Lua = {}
+            }
+         })
+      end,
+   },
 }
+
+
+
+
+
+
+
+
+
